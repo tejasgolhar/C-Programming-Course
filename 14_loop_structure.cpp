f9a2da2b7412f3a474459c79418e428abb16a69b
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-01-14 13:34:28 +0530

    commit

---
+++ 14_loop_structure.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int i=1;
+    /*
+    while (i<=40)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+   
+
+  while ()
+  {
+
+  }
+  */
+  //Multiplication Table of 2 using do while loop
+
+  do
+  {
+    cout<< 2*i<< 3*i<< 4*i<< 5*i<<endl;
+    i++;
+  } while (i<=10);
+  
+
+    return 0;
+}
