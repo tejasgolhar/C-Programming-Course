42103777b306bf34e64fc4b0a5d1b1d3ddb8646a
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-01-15 15:55:23 +0530

    commit

--- 14_loop_structure.cpp
+++ 14_loop_structure.cpp
@@ -1,30 +1,61 @@
 #include<iostream>
+#include<string>
 using namespace std;
-int main()
-{
-    
-    int i=1;
-    /*
-    while (i<=40)
-    {
-        cout<<i<<endl;
-        i++;
-    }
-   
 
-  while ()
+int main(){
+
+
+//    Print Your name 10 times on the console window using loops in c++
+
+
+  int i;
+  string name;
+  cout<<"Enter the name to be printed 10* on the console"<<endl;
+  cin>>name;
+  cout<<"\n";
+  
+               //**********FOR LOOP**********
+
+ /* 
+
+  for ( i = 0; i < 10; i++ )
   {
-
+    cout<<name<<endl;
   }
-  */
-  //Multiplication Table of 2 using do while loop
 
-  do
-  {
-    cout<< 2*i<< 3*i<< 4*i<< 5*i<<endl;
-    i++;
-  } while (i<=10);
+
+
+  */
+
+               //*******WHILE LOOP********
+ 
+  /*
+      while (i<10)
+      {
+        cout<<i+1<<". "<<name<<endl;
+        i++;
+      }
+    
+  */
+
+
+
+
+           //********* DO WHILE LOOP ********
+    
+/*
+    i=0;
+
+    do{
+      cout<<i+1<<". "<<name<<endl;
+      i++;
+    }while(i<10);
+    
+*/
+    
+return 0;
+}
+
   
 
-    return 0;
-}
+ 
