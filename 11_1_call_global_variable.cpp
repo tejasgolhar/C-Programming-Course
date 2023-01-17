8811fe13b8cb1a7715108054adc9af3a11671627
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-01-17 23:07:30 +0530

    commit

--- 11_1_call_global_variable.cpp
+++ 11_1_call_global_variable.cpp
@@ -4,16 +4,28 @@ using namespace std;
 int a=56;
 
 int main(){
+
+
+    //                 ******Build in Data types********
+    // int a, b, c;
+    // cout<<"Enter the value of a:"<<endl;
+    // cin>>a;
+    // cout<<"Enter the value of b:"<<endl;
+    // cin>>b;
+    // c = a + b;
+    // cout<<"The sum is "<<c<<endl;
+    // cout<<"The global c is "<<::c;
+
     
-    //*****Call the value stored in the global variable*****
+    //            *****   Call the value stored in the global variable    *****
     
     int a=45;   //regenerated the variable with same name
 
     //to print the value in a when called normally
-    cout<<"Enter the value of varible 'a':\n"<<a<<endl;
+    cout<<"Enter the value of varible 'a':\n"<<a<<endl; 
 
     //to access the value of variable stored globally
-    cout<<"Enter the value of varible 'a' stored globally:\n"<<::a<<endl;
+    cout<<"Enter the value of varible 'a' stored globally:\n"<<::a<<endl; 
 
 
     return 0;
---
+++ 11_4_typecasting.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      
+      
+     // *************   Typecasting in c++  **************** 
+    
+    
+    int a = 45;   
+    float b = 45.46;
+    
+    cout<<"The value of a is "<<(float)a<<endl;
+    cout<<"The value of a is "<<float(a)<<endl;
+
+    cout<<"The value of b is "<<(int)b<<endl;
+    cout<<"The value of b is "<<int(b)<<endl;
+   
+   
+    int c = int(b);
+
+    cout<<"The expression is "<<a + b<<endl;
+    cout<<"The expression is "<<a + int(b)<<endl;
+    cout<<"The expression is "<<a + (int)b<<endl;
+
+    return 0;
+}
